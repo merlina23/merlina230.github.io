7f0a529dae8a872123e03c4fc5370a486e232ef4
merlina23 <126361540+merlina23@users.noreply.github.com>
2023-03-07 11:23:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//boton fecha de egreso
   document.getElementById("Fechag").addEventListener('click', function() {  
   document.getElementById("demo").style.display= "block";
   
@@ -6,22 +6,23 @@
   document.getElementById("ocultar").addEventListener('click', function() {   
   document.getElementById("demo").style.display = "none";
   });
- 
+ //cambio de color apartado "conocimientos
   const collection = document.getElementsByClassName("programas");
   for (let i = 0; i < collection.length; i++) {
   collection[i].style.color = "grey";
   }
-  
+
+  //boton cambio de color de fondo
   document.getElementById("color").addEventListener('click', function() {   
     document.body.style.backgroundColor = "pink";
     });
     
     
- 
+ //reestablecimiento color de fondo
     document.getElementById("reestablecer").addEventListener('click', function() {   
       document.body.style.backgroundColor = "whitesmoke";
     });
-
+//cambio de color titulo 2 
     document.getElementById("profesion").addEventListener('mouseover', function() {   
       document.getElementById("profesion").style.color = "blue";
     });
